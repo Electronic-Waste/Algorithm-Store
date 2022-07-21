ec91db930402b6622ae9842eb06b2fca20545cf8
Electronic-Waste <2690692950@qq.com>
2022-07-22 01:42:52 +0800

    Accomplish transferring file function in frontend

--- front_end/src/services/FetchUpdate.js
+++ front_end/src/services/FetchUpdate.js
@@ -1,28 +1,18 @@
 import React from "react";
 import Settings from "../utils/Settings";
-function FetchUpdate(props) {
-    let title = props.title;
-    let type = props.type;
-    let author = props.author;
-    let url= props.url;
-    let description= props.description;
-    let tag= props.tag;
-    const ans=fetch(Settings.url + Settings.update_path, {
-        method: "POST",
-        headers: {
+function FetchUpdate(data) {
 
-        },
-        body: JSON.stringify({
-            title: title,
-            type: type,
-            author: author,
-            url: url,
-            description: description,
-            tag: tag,
-        }),
+    fetch(Settings.url + Settings.update_path, {
+        method: "POST",
+        // headers: {
+        //     'Content-Type': 'multipart /form-data'
+        // },
+        // processData: false,
+        // contentType: false,
+        body: data
+    }).then(data => {
+
     });
 
-
-    return !!ans?null:ans.json();
 }
 export default FetchUpdate;
